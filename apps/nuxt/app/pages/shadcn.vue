03308b68590a5b9ce60dde04d955fd9392f683df
doooooooomiki <dominic.kirste@gmail.com>
2025-03-02 09:21:47 +0100

    🚧

--- apps/nuxt/app/pages/shadcn.vue
+++ apps/nuxt/app/pages/shadcn.vue
@@ -4,5 +4,11 @@
       <AvatarImage src="https://github.com/nuxt.png" alt="@radix-vue" />
       <AvatarFallback>CN</AvatarFallback>
     </Avatar>
+    <Alert>
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components to your app using the cli.
+      </AlertDescription>
+    </Alert>
   </section>
 </template>
