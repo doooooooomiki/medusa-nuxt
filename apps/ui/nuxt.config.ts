20416c6f34142f3eb278b6c42365b738437d385e
doooooooomiki <dominic.kirste@gmail.com>
2025-03-02 21:11:11 +0100

    ♻️

--- apps/ui/nuxt.config.ts
+++ apps/ui/nuxt.config.ts
@@ -1,4 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-03-02",
 
@@ -10,4 +12,11 @@ export default defineNuxtConfig({
     prefix: "",
     componentDir: "./components",
   },
+
+  components: [
+    {
+      path: join(dirname(fileURLToPath(import.meta.url)), "./components"),
+      extensions: [".vue"],
+    },
+  ],
 });
