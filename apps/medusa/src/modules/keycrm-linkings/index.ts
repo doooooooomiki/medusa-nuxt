3d6718a3cdc54cfad26c02bb9d7303e4748d872c
doooooooomiki <dominic.kirste@gmail.com>
2025-03-05 20:36:48 +0100

    🔥

--- apps/medusa/src/modules/keycrm-linkings/index.ts
+++
@@ -1,8 +0,0 @@
-import KeycrmLinkingModuleService from "./service";
-import { Module } from "@medusajs/framework/utils";
-
-export const KEYCRM_LINKING_MODULE = "keycrmlinking";
-
-export default Module(KEYCRM_LINKING_MODULE, {
-  service: KeycrmLinkingModuleService,
-});
