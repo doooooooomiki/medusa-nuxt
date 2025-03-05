96726d8b6fed9f93bdf5175a67230b0b5d37e48d
doooooooomiki <dominic.kirste@gmail.com>
2025-03-05 20:36:48 +0100

    🔥

--- apps/medusa/src/modules/keycrm-linkings/models/linking.ts
+++
@@ -1,9 +0,0 @@
-import { model } from "@medusajs/framework/utils";
-
-const Linking = model.define("linking", {
-  id: model.id().primaryKey(),
-  keycrm_product_id: model.number().unique(),
-  handle: model.text().unique().searchable(),
-});
-
-export default Linking;
--- apps/medusa/src/modules/keycrm-linkings/service.ts
+++
@@ -1,8 +0,0 @@
-import { MedusaService } from "@medusajs/framework/utils";
-import Linking from "./models/linking";
-
-class KeycrmLinkingModuleService extends MedusaService({
-  Linking,
-}) {}
-
-export default KeycrmLinkingModuleService;
