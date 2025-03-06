2f9180a86b21dfa4524b965707d6eef4e78ff962
doooooooomiki <dominic.kirste@gmail.com>
2025-03-06 10:37:54 +0100

    🎨

---
+++ apps/nuxt/app/pages/collections/[handle].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const { data: product, error } = await useFetch<any>(
+  route.params.handle as string,
+  {
+    baseURL: "/api/collections/",
+  }
+);
+</script>
+
+<template>{{ route.params.handle }}</template>
