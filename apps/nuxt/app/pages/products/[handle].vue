0df50ff62d5fb17eb90c0ca67379737e91a44fe8
doooooooomiki <dominic.kirste@gmail.com>
2025-03-10 17:36:32 +0100

    🚧

--- apps/nuxt/app/pages/products/[handle].vue
+++ apps/nuxt/app/pages/products/[handle].vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 const route = useRoute();
+const handle = computed(() => route.params.handle as string);
+
 // TODO: throw if route.params.handle is anything else than string
 
-const { data: product, error } = await useFetch<any>(
-  route.params.handle as string,
-  {
-    baseURL: "/api/products/",
-  }
-);
+const { data: product, error } = await useFetch<any>(handle, {
+  baseURL: "/api/products/",
+});
 
 if (error.value) {
   throw createError({ statusCode: 404, statusMessage: "Page Not Found" });
